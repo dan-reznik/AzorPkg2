bbef3ef6d4f3c5cc4db52627432ee130af003716
dan-reznik <dreznik@gmail.com>
2018-07-03 13:55:18 +0000

    marked imgs

--- R/validate.R
+++ R/validate.R
@@ -19,7 +19,8 @@ validate_sex <- function(sex,
                          # can be vectorized
                          value,
                          val_min_hom,val_max_hom,
-                         val_min_mul,val_max_mul) {
+                         val_min_mul,val_max_mul,
+                         png_marked_homem,png_marked_mulher) {
   val_min <- NA_real_
   val_max <- NA_real_
   sex <- str_to_upper(sex)
@@ -27,38 +28,47 @@ validate_sex <- function(sex,
     if(sex=="M") {
       val_min <- val_min_hom
       val_max <- val_max_hom
+      png_marked <- png_marked_homem
     } else { # "F"
       val_min <- val_min_mul
       val_max <- val_max_mul
+      png_marked <- png_marked_mulher
     }
   }
 
   if(is.na(value)|value<0|is.na(val_min)|is.na(val_max))
-    list(value=value,min=val_min,max=val_max,valid=NA)
+    list(value=value,min=val_min,max=val_max,ref_png=png_marked,valid=NA)
   else
     list(value=value,
          min=val_min,max=val_max,
+         ref_png=png_marked,
          valid=between(value,val_min,val_max))
 }
+
+img_path <- function(img_name) str_c("https://dan-reznik.ocpu.io/AzorPkg2/marked/",
+                                     img_name,
+                                     ".png")
 
 validate_age <- function(df_key_matched,sex,age_days,
                          value) {
   if(nrow(df_key_matched)==1)
     df_age_matched <- df_key_matched
   else {
-    if(is.na(age_days)) return(list(value=value,min=NA,max=NA,valid=NA))
+    if(is.na(age_days)) return(list(value=value,min=NA,max=NA,ref_png=NA,valid=NA))
     df_age_matched <- df_key_matched %>%
       dplyr::filter(map2_lgl(age_min_days,age_max_days,
                              ~between(age_days,.x,.y)))
     }
+  # there should be only one match
   assert_that(nrow(df_age_matched)==1)
-  # there should be only one line here
   validate_sex(sex,
                value,
                val_min_hom=df_age_matched$homem_min,
                val_max_hom=df_age_matched$homem_max,
                val_min_mul=df_age_matched$mulher_min,
-               val_max_mul=df_age_matched$mulher_max)
+               val_max_mul=df_age_matched$mulher_max,
+               png_marked_homem=img_path(df_age_matched$imagem_regra_homem),
+               png_marked_mulher=img_path(df_age_matched$imagem_regra_mulher))
 }
 
 validate_exam_result <- function(sex,
@@ -66,13 +76,13 @@ validate_exam_result <- function(sex,
                                  exam_id_valor,
                                  exam_value) {
   if(is.null(exam_id_valor)|is.na(exam_id_valor))
-    list(value=exam_value,min=NA,max=NA,valid=NA)
+    list(value=exam_value,min=NA,max=NA,ref_png=NA,valid=NA)
   else {
     # exam_key <- exam_key %>% str_trim %>% str_to_lower
     df_chave <- df_ref_rules %>% # global
       dplyr::filter(id_valor==exam_id_valor)
     if(nrow(df_chave)==0)
-      list(value=exam_value,min=NA,max=NA,valid=NA)
+      list(value=exam_value,min=NA,max=NA,ref_png=NA,valid=NA)
     # '{"min":null,"max":null,"valid":null}'
     else
       validate_age(df_chave,sex,age_in_days_at_exam,exam_value)
@@ -103,8 +113,9 @@ id_to_snippet <- function(id) df_ref_dict %>%
 #' @export
 validate_exams <- function(sex,
                            birth_ymd,exam_ymd,
-                           exam_id_vec,exam_value_vec,
-                           ref_png=F) {
+                           exam_id_vec,exam_value_vec
+                           #,ref_png=F
+                           ) {
   #to do:
   dl <- length(exam_id_vec)-length(exam_value_vec)
   if(dl>0) exam_id_vec <- exam_id_vec[1:length(exam_value_vec)]
@@ -122,9 +133,9 @@ validate_exams <- function(sex,
     bind_cols(
       map2_dfr(exam_id_vec,exam_value_vec,
                ~validate_exam_result(sex,age_in_days_at_exam,.x,.y)))
-  if (ref_png)
-    df %>% bind_cols(ref_png=id_to_snippet(exam_id_vec))
-  else
+  # if (ref_png)
+  #   df %>% bind_cols(ref_png=id_to_snippet(exam_id_vec))
+  # else
     df
 }
 

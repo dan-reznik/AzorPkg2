4a2283d27da3a4cf69ca4d79eee9f6cb600551f9
dan-reznik <dreznik@gmail.com>
2018-07-20 20:21:04 +0000

    added df_exames, mudanças de acentos etc

--- data-raw/load_dict_and_rules.R
+++ data-raw/load_dict_and_rules.R
@@ -2,6 +2,10 @@
 df_ref_dict <- readr::read_rds("data-raw/df_ref_dict.rds")
 # Business Rules (exam,sex,age)=>val_min,val_max
 df_ref_rules <- readr::read_rds("data-raw/df_ref_rules.rds")
+# List of exams
+df_exames <- readr::read_rds("data-raw/df_exames.rds")
 
-devtools::use_data(df_ref_dict,df_ref_rules,
+devtools::use_data(df_ref_dict,
+                   df_ref_rules,
+                   df_exames,
                    overwrite = T,internal = T)
